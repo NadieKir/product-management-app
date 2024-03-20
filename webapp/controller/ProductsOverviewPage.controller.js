06572bc7b2ed1bb3d875208cc7ed19397bb92773
Nadezhda Kireyenko <nadiekir@gmail.com>
2024-03-20 19:38:05 +0300

    Add List Report filter bar controls

--- webapp/controller/ProductsOverviewPage.controller.js
+++ webapp/controller/ProductsOverviewPage.controller.js
@@ -5,17 +5,25 @@ sap.ui.define(["./BaseController", "sap/ui/model/json/JSONModel"], function (Bas
     onInit: function () {
       const oView = this.getView();
       const oProducts = new JSONModel("model/products.json");
+      const oCategories = new JSONModel("model/categories.json");
+      const oSuppliers = new JSONModel("model/suppliers.json");
       const oProductsOverviewViewModel = new JSONModel({ selectedProducts: [] });
 
+      const sCurrentTimestamp = new Date();
+
       oView.setModel(oProducts, "products");
+      oView.setModel(oCategories, "categories");
+      oView.setModel(oSuppliers, "suppliers");
       oView.setModel(oProductsOverviewViewModel, "productsOverviewView");
+
+      this.byId("idReleaseDateDateRangeSelection").setMaxDate(sCurrentTimestamp);
     },
 
     onColumnListItemPress: function () {},
 
     onTableSelectionChange: function () {
       const oViewModel = this.getView().getModel("productsOverviewView");
-      const aSelectedItems = this.getView().byId("idProductsTable").getSelectedItems();
+      const aSelectedItems = this.byId("idProductsTable").getSelectedItems();
 
       oViewModel.setProperty("/selectedProducts", aSelectedItems);
     },
