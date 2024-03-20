27c46cc324a21f79dc7a5bb2b70c81887ca91550
Nadezhda Kireyenko <nadiekir@gmail.com>
2024-03-20 17:05:26 +0300

    Implement delete button disabled without selection

--- webapp/controller/ProductsOverviewPage.controller.js
+++ webapp/controller/ProductsOverviewPage.controller.js
@@ -1,38 +1,23 @@
-// sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-//   "use strict";
+sap.ui.define(["./BaseController", "sap/ui/model/json/JSONModel"], function (BaseController, JSONModel) {
+  "use strict";
 
-//   return Controller.extend("nadzeya.kireyenka.app.controller.ProductsOverviewPage", {
-//     onNavToProductDetailsPage: function () {
-//       this.getOwnerComponent().getRouter().navTo("ProductDetailsPage", { productId: "1" });
-//     },
-//   });
-// });
+  return BaseController.extend("nadzeya.kireyenka.app.controller.ProductsOverviewPage", {
+    onInit: function () {
+      const oView = this.getView();
+      const oProducts = new JSONModel("model/products.json");
+      const oProductsOverviewViewModel = new JSONModel({ selectedProducts: [] });
 
-sap.ui.define(
-  [
-    "sap/ui/thirdparty/jquery",
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast",
-    "sap/ui/core/format/DateFormat",
-    "sap/base/Log",
-    "sap/ui/core/Fragment",
-    "sap/ui/core/date/UI5Date",
-  ],
-  function (jQuery, Controller, JSONModel, MessageToast, DateFormat, Log, Fragment, UI5Date) {
-    "use strict";
+      oView.setModel(oProducts, "products");
+      oView.setModel(oProductsOverviewViewModel, "productsOverviewView");
+    },
 
-    return Controller.extend("nadzeya.kireyenka.app.controller.ProductsOverviewPage", {
-      onInit: function () {
-        const oView = this.getView();
-        const oProducts = new JSONModel("model/products.json");
-        const oProductsOverviewViewModel = new JSONModel();
+    onColumnListItemPress: function () {},
 
-        oView.setModel(oProducts, "products");
-        oView.setModel(oProductsOverviewViewModel, "productsOverviewView");
-      },
+    onTableSelectionChange: function () {
+      const oViewModel = this.getView().getModel("productsOverviewView");
+      const aSelectedItems = this.getView().byId("idProductsTable").getSelectedItems();
 
-      onProductsTableItemPress: function () {},
-    });
-  }
-);
+      oViewModel.setProperty("/selectedProducts", aSelectedItems);
+    },
+  });
+});
